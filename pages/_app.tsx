029afce9c3c587c977cec2b549b4cfd381adf63f
joacoguzmanz <joaquingusman77@gmail.com>
2023-02-23 19:45:15 -0300

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
+import { Raleway } from "@next/font/google";
 import type { AppProps } from 'next/app'
 
+const raleway = Raleway({ subsets: ['latin'] });
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <main className={raleway.className}>
+        <Component {...pageProps} />
+      </main>
+  )
 }
